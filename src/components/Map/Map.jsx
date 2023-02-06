a03a23598938eda0449aab22dcc01ab8502de4b0
Sachyaaa <84377689+Sachyaaa@users.noreply.github.com>
2023-02-06 19:47:35 +0530

    Places and list component are modified

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React from "react"; 
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography,useMediaQuery } from "@material-ui/core";
+import { LocationCityOutlinedIcon } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from './styles';
 
 const Map = ()=>{
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const cordinates = {lat:0, lng:0};
     return(
-        <h1>Map</h1>
+        <div className={classes
+        .mapContainer}>
+            <GoogleMapReact
+             bootstrapURLKeys={{key:'AIzaSyAh79vb4f_QZuyLUukLtV-vP8jLKVDJsTI'}}
+             defaultCenter={cordinates}
+             center={cordinates}
+             defaultZoom={14}
+             margin={[50,50,50,50]}
+             options={''}
+             onChange={''}
+             onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
